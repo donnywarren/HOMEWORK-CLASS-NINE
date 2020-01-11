50a06ae5626f0bf6c58e8edf8ef0fb6afa4e0718
donnywarren <51206190+donnywarren@users.noreply.github.com>
2020-01-11 08:17:56 -0500

    setting variables

---
+++ index.js
@@ -0,0 +1,16 @@
+var result-box-red = document.createElement("div");
+result-box-red.id = "result-box-red";
+result-box-red.className = "result-box red";
+
+
+
+var result-box-green = document.createElement("div");
+result-box-green.id = "result-box-green";
+result-box-green.className = "result-box green";
+
+
+
+
+var result-box-blue = document.createElement("div");
+result-box-blue.id = "result-box-blue";
+result-box-blue.className = "result-box blue";
